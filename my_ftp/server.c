806a515ea082b8dfea51ba090bef603c2b64e08c
Hugo ARNAL <hugo@hugoarnal.com>
2026-02-18 00:15:45 +0100

    feat: (most of) step 5
    Didn't do it all because it's kind of long and I think I get the gist of it

--- my_ftp/server.c
+++ my_ftp/server.c
@@ -9,6 +9,9 @@
 #include <string.h>
 #include <stdbool.h>
 #include <poll.h>
+#include <fcntl.h>
+
+#define CRLF "\r\n"
 
 struct poller_s {
     struct pollfd *fds;
@@ -19,6 +22,33 @@ struct poller_s {
     // size of the array
     unsigned int size;
 };
+
+struct ftp_s {
+    struct poller_s *poller;
+
+    // CONTROL socket
+    int control_fd;
+
+    // DATA socket
+    int data_fd;
+    int data_accepted_fd;
+};
+
+// idc
+int socket_init(int port);
+
+struct ftp_s *ftp_init(void)
+{
+    struct ftp_s *ftp = malloc(sizeof(struct ftp_s));
+
+    if (ftp == NULL)
+        return NULL;
+    ftp->data_fd = -1;
+    ftp->data_accepted_fd = -1;
+    ftp->control_fd = -1;
+    ftp->poller = NULL;
+    return ftp;
+}
 
 struct poller_s *poller_init(unsigned int initial_size, int socket_fd)
 {
@@ -74,37 +104,145 @@ void new_client_handler(struct poller_s *poller, int socket_fd)
     printf("Connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 }
 
-void client_handler(struct poller_s *poller, int i, int socket_fd)
+void client_quit(struct ftp_s *ftp, int fd, int socket_fd, unsigned int i)
+{
+    if (fd != socket_fd) {
+        if (close(fd) == -1)
+            perror("close");
+        poller_fd_delete(ftp->poller, i);
+        i--;
+    }
+}
+
+char *get_formatted_ip(char *ip)
+{
+    size_t len = strlen(ip);
+    for (size_t i = 0; i < len; i++) {
+        if (ip[i] == '.') {
+            ip[i] = ',';
+        }
+    }
+    return ip;
+}
+
+void get_formatted_port(in_port_t *port, int *p1, int *p2)
+{
+    uint16_t net_port = ntohs(*port);
+    *p1 = net_port / 256;
+    *p2 = net_port - (*p1 * 256);
+}
+
+void client_handler(struct ftp_s *ftp, unsigned int *i, int socket_fd)
 {
     size_t a = 256;
     char buffer[a];
-    int fd = poller->fds[i].fd;
+    int fd = ftp->poller->fds[*i].fd;
 
-    if (read(fd, buffer, a) <= 0) {
-        if (fd != socket_fd) {
-            if (close(fd) == -1)
-                perror("close");
-            poller_fd_delete(poller, i);
-            i--;
-        }
+    int bytes_read = read(fd, buffer, a);
+    if (bytes_read <= 0) {
+        client_quit(ftp, fd, socket_fd, *i);
     } else {
-        const char *str = "I lost the game :3\n";
-        if (write(fd, str, strlen(str)) == -1) {
-            perror("write");
+        buffer[bytes_read] = 0;
+        if (strcmp(buffer, "PASV"CRLF) == 0) {
+            ftp->data_fd = socket_init(0);
+            if (ftp->data_fd == -1) {
+                fprintf(stderr, "yeah i just couldn't bind fuck it\n");
+                exit(84);
+            }
+            struct sockaddr_in socket_info;
+            socklen_t socket_info_len = sizeof(socket_info);
+            getsockname(ftp->data_fd, (struct sockaddr *)&socket_info, &socket_info_len);
+
+            int p1, p2;
+            get_formatted_port(&socket_info.sin_port, &p1, &p2);
+
+            printf("The fucking port: %d\n", ntohs(socket_info.sin_port));
+
+            struct sockaddr_in client_addr;
+            socklen_t client_addr_len = sizeof(client_addr);
+            printf("before accept\n");
+            ftp->data_accepted_fd = accept(ftp->data_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+            printf("after accept\n");
+
+            if (ftp->data_accepted_fd == -1) {
+                perror("accept");
+                return;
+            }
+
+            char buffer2[2048];
+            snprintf(buffer2,
+                2048,
+                "227 Entering Passive Mode (%s,%d,%d)"CRLF,
+                get_formatted_ip(inet_ntoa(socket_info.sin_addr)),
+                p1, p2);
+            if (write(fd, buffer2, strlen(buffer2)) == -1) {
+                perror("write");
+                return;
+            }
+        } else if (strncmp(buffer, "RETR ", 5) == 0) {
+            // TODO: I'm pretty sure I should be forking here!
+            if (ftp->data_fd == -1) {
+                fprintf(stderr, "create data stream pls\n");
+                return;
+            }
+            char *filename = strrchr(buffer, ' ') + 1;
+            filename[strlen(filename) - 1] = 0;
+            filename[strlen(filename) - 1] = 0;
+            printf("fucking filename: %s\nsize: %zu\n", filename, strlen(filename));
+
+            // char *filename = "Makefile";
+            int file_fd = open(filename, O_RDONLY);
+
+            if (file_fd == -1) {
+                perror("open");
+                return;
+            }
+
+            const char *status = "150";
+            if (write(fd, status, strlen(status)) == -1) {
+                perror("write");
+                return;
+            }
+
+            // fuck it only buffer, no stat
+            char filebuffer[4096];
+            int bytes_read_file = read(file_fd, filebuffer, 4096);
+            if (bytes_read_file == -1) {
+                perror("read");
+                return;
+            }
+
+            if (write(ftp->data_accepted_fd, filebuffer, strlen(filebuffer)) == -1) {
+                perror("write");
+                return;
+            }
+
+            close(file_fd);
+            close(ftp->data_fd);
+            close(ftp->data_accepted_fd);
+            ftp->data_fd = -1;
+            ftp->data_accepted_fd = -1;
+        } else if (strcmp(buffer, "QUIT"CRLF) == 0) {
+            client_quit(ftp, fd, socket_fd, *i);
+        } else {
+            const char *str = "I lost the game :3\n";
+            if (write(fd, str, strlen(str)) == -1) {
+                perror("write");
+            }
         }
     }
 }
 
-void poller_handler(struct poller_s *poller, int socket_fd)
+void poller_handler(struct ftp_s *ftp)
 {
-    unsigned int amount = poller->amount;
+    unsigned int amount = ftp->poller->amount;
 
     for (unsigned int i = 0; i < amount; i++) {
-        if (poller->fds[i].revents & POLLIN) {
-            if (poller->fds[i].fd == socket_fd) {
-                new_client_handler(poller, socket_fd);
+        if (ftp->poller->fds[i].revents & POLLIN) {
+            if (ftp->poller->fds[i].fd == ftp->control_fd) {
+                new_client_handler(ftp->poller, ftp->control_fd);
             } else {
-                client_handler(poller, i, socket_fd);
+                client_handler(ftp, &i, ftp->control_fd);
             }
         }
     }
@@ -154,7 +292,18 @@ int main(int argc, char **argv)
     if (socket_fd < 0)
         return 84;
 
+    struct ftp_s *ftp = ftp_init();
+
+    if (ftp == NULL)
+        return 84;
+
     struct poller_s *poller = poller_init(10, socket_fd);
+
+    if (poller == NULL)
+        return 84;
+
+    ftp->poller = poller;
+    ftp->control_fd = socket_fd;
 
     while (true) {
         int poll_result = poll(poller->fds, poller->amount, -1);
@@ -164,7 +313,7 @@ int main(int argc, char **argv)
             return 84;
         }
 
-        poller_handler(poller, socket_fd);
+        poller_handler(ftp);
     };
 
     return 0;
