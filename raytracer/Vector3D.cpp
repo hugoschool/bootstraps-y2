cab6d065e453575a903fae64c2445dec6a440a5b
Hugo ARNAL <hugo@hugoarnal.com>
2026-04-20 19:41:22 +0200

    fix: remove inline in Vector3D

--- raytracer/Vector3D.cpp
+++ raytracer/Vector3D.cpp
@@ -5,12 +5,12 @@ Math::Vector3D::Vector3D(double x, double y, double z) : x(x), y(y), z(z)
 {
 }
 
-inline double Math::Vector3D::length()
+double Math::Vector3D::length()
 {
     return std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2));
 }
 
-inline double Math::Vector3D::dot(Vector3D &vector)
+double Math::Vector3D::dot(Vector3D &vector)
 {
     return x * vector.x + y * vector.y + z * vector.z;
 }
