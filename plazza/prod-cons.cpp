717563cedb5ca02f9d323cead4d0536f0eb069df
Hugo ARNAL <hugo@hugoarnal.com>
2026-05-14 11:37:25 +0200

    feat: step 2 - exercise 6

--- plazza/prod-cons.cpp
+++ plazza/prod-cons.cpp
@@ -1,6 +1,10 @@
+#include <chrono>
+#include <cstdlib>
+#include <memory>
 #include <mutex>
 #include <queue>
-
+#include <thread>
+#include <iostream>
 
 class ISafeQueue {
     public:
@@ -38,3 +42,80 @@ class SafeQueue : public ISafeQueue {
 
         std::queue<int> _queue;
 };
+
+using namespace std::chrono_literals;
+
+class Consumer {
+    public:
+        Consumer() = delete;
+        Consumer(std::size_t i, SafeQueue &queue) : _queue(queue)
+        {
+            _thread = std::thread([this, i]() {
+                std::cerr << "Created consumer " << i << std::endl;
+                while (true) {
+                    int value;
+
+                    if (_queue.tryPop(value) == false)
+                        std::this_thread::sleep_for(5s);
+                    else
+                        std::cout << "Unstacked value by " << i << ": " << value << std::endl;
+                }
+            });
+        }
+        ~Consumer() = default;
+
+        void join()
+        {
+            _thread.join();
+        }
+
+    private:
+        SafeQueue &_queue;
+        std::thread _thread;
+};
+
+class Producer {
+    public:
+        Producer() = delete;
+        Producer(std::size_t i, SafeQueue &queue) : _queue(queue)
+        {
+            _thread = std::thread([this, i]() {
+                std::cerr << "Created producer " << i << std::endl;
+                while (true) {
+                    _queue.push(std::rand() % 500);
+                    std::this_thread::sleep_for(1s);
+                }
+            });
+        }
+        ~Producer() = default;
+
+        void join()
+        {
+            _thread.join();
+        }
+
+    private:
+        SafeQueue &_queue;
+        std::thread _thread;
+};
+
+int main(void)
+{
+    std::srand(std::time(nullptr));
+
+    const std::size_t consumerNb = 2;
+    const std::size_t producerNb = 5;
+    SafeQueue queue;
+    std::unique_ptr<Consumer> consumers[consumerNb];
+    std::unique_ptr<Producer> producers[producerNb];
+
+    for (std::size_t i = 0; i < consumerNb; i++)
+        consumers[i] = std::make_unique<Consumer>(i, queue);
+    for (std::size_t i = 0; i < producerNb; i++)
+        producers[i] = std::make_unique<Producer>(i, queue);
+
+    for (std::size_t i = 0; i < consumerNb; i++)
+        consumers[i]->join();
+    for (std::size_t i = 0; i < producerNb; i++)
+        producers[i]->join();
+}
